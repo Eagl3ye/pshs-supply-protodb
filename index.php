5e600b445f731779cd9f3ddce9b3a00a96fe4639
Eagl3ye <vincejaphethloable@gmail.com>
2020-02-23 02:01:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
-	include("templates/header.htm"); 
-	include("templates/index.htm"); 
-	include("templates/footer.htm");
+  include("templates/header.php");
+  $action = 'index';
+  $disallow_paths = array('header', 'footer');
+  if (!empty($_GET['action'])) {
+    $tmp_action = basename($_GET['action']);
+    if (!in_array($tmp_action, $disallow_paths) && file_exists("templates/{$tmp_action}.php")) {
+      $action = $tmp_action;
+    } else {
+      $action = 'notfound';
+    }
+    include("templates/$action.php"); //allowed page
+  }
+  include("templates/$action.php"); //disallowed/unavailable page
+  include("templates/footer.php");
 ?>
