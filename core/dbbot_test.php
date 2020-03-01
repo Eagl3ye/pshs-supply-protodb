97ffcf14780e4e59df54822d0d3c5d0eaa82ba03
Eagl3ye <vincejaphethloable@gmail.com>
2020-03-01 13:58:49 +0800

    Create dbbot_test.php

---
+++ core/dbbot_test.php
@@ -0,0 +1,7 @@
+$conn = pg_connect(getenv("DATABASE_URL"));
+$result = pg_query($conn, "SELECT * FROM kidz");
+$multiarr = pg_fetch_all($result);
+foreach ($multiarr as $arr) {
+    echo "$arr[usr_id] <br>";  
+    echo "$arr[mono] <br><br>";
+}
