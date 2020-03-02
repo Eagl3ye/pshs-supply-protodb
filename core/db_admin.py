d53938551746e9cfedcf6274c970ba8e209d9862
Eagl3ye <vincejaphethloable@gmail.com>
2020-03-02 11:47:22 +0800

    Update db_admin.py

--- core/db_admin.py
+++ core/db_admin.py
@@ -2,7 +2,7 @@ import os														#OS
 import time														#TIME
 import psycopg2													#DATABASE HANDLING
 DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require'
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
 cur.execute("CREATE TABLE Users (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, firstname VARCHAR(30) NOT NULL, lastname VARCHAR(30) NOT NULL, email VARCHAR(50), isGuest BOOLEAN, reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)");
 conn.commit()
