cac447aa41777bdd4c88f66020205a80d8adc762
Eagl3ye <vincejaphethloable@gmail.com>
2020-03-02 11:55:33 +0800

    Removed db_admin.py

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 worker1: php index.php
-worker2: python3 core/db_admin.py
--- core/db_handler.php
+++ core/db_handler.php
@@ -1,4 +1,4 @@
-<?php    
+<?php
 	$conn = pg_connect(getenv("DATABASE_URL"));
 	$sql = "CREATE TABLE Users (
 		id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -8,11 +8,6 @@
 		isGuest BOOLEAN,
 		reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 		)";
-	$query = pg_query($conn, $sql);
-	if (!$result) {
-  		echo "An error occurred.\n";
-  		exit;
-	}
-
+	pg_query($conn, $sql);
 	pg_close($conn);
 ?>
