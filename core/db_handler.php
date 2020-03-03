8c371152f31f341cb840941c6ba8603cd50f6101
Eagl3ye <vincejaphethloable@gmail.com>
2020-03-03 15:38:06 +0800

    Fixing Handler

--- core/db_handler.php
+++ core/db_handler.php
@@ -1,14 +1,14 @@
 <?php
 	$conn = pg_connect(getenv("DATABASE_URL"));
-	$sql = "CREATE TABLE Users (
-		id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-		firstname VARCHAR(30) NOT NULL,
-		lastname VARCHAR(30) NOT NULL,
-		email VARCHAR(50),
-		isGuest BOOLEAN,
-		reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-		)";
+	$sql = "CREATE TABLE Users";
 	pg_query($conn, $sql);
-	pg_close($conn);
+	//(
+	//	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	//	firstname VARCHAR(30) NOT NULL,
+	//	lastname VARCHAR(30) NOT NULL,
+	//	email VARCHAR(50),
+	//	isGuest BOOLEAN,
+	//	reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+	//	)
 ?>
- 	
+
